1d70efcc5787e2fb5070eaf90d55b2e979a312b2
Mano <lemmensmano@gmail.com>
2023-06-16 15:20:41 +0200

    simple blog post styling

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,7 +1,30 @@
-import { tw } from "twind";
+import { apply, tw } from "twind";
+import { css } from "twind/css"
 import { post } from "../utils/Markdown.ts";
 
 export function Post(props: { post: post }) {
   const why = { __html: props.post.content };
-  return <div dangerouslySetInnerHTML={why} class={tw`w-32 p-4`}></div>;
+
+  // I dont like this
+  // but I don't know 
+  // how else I can do this
+  const style = css({
+    '& > h1' : {
+      fontSize: '1.5em'
+    },
+
+    '& > h2' : {
+      fontSize: '1.25em'
+    },
+
+    '& > h3' : {
+      fontSize: '1.1em'
+    },
+
+    '& > p' : { 
+      margin: '1rem'
+    }
+  })
+
+  return <div dangerouslySetInnerHTML={why} class={tw`w-full p-4${apply(style)}`}></div>;
 }
